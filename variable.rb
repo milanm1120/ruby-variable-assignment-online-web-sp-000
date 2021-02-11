be9bd48efa714485606f76331fc426404afc1d9a
Milan Mody <milanm1120@gmail.com>
2021-02-11 01:26:22 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,4 +1,25 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-
 greeting = "Hello World"
+
+
+
+
+def question(answer)
+  puts "A group of flamingos is called a #{answer}."
+end
+
+question("Flamboyance")
+
+
+def Introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+Introduction("Milan")
+
+def intro_with_a_language(name, language)
+  puts "Hi my name is #{name}, and I am learning to program in #{language}."
+end
+
+intro_with_a_language("Milan", "Ruby")
